b950bad24e1af6fec579b1edc70cd21508fe8d77
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2011-09-17 18:41:49 +0530

    Fixed bug in py code

--- exampleapp.py
+++ exampleapp.py
@@ -135,7 +135,7 @@ def index():
             friends=friends, photos=photos, app_friends=app_friends, app=app,
             me=me, POST_TO_WALL=POST_TO_WALL, SEND_TO=SEND_TO)
         '''
-        return render_template(index1.html,app=app)
+        return render_template('index1.html',app=app)
     else:
         print oauth_login_url(next_url=get_home())
         return redirect(oauth_login_url(next_url=get_home()))
